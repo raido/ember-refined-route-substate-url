e0877e81b5d0d084db4e37248a43f2d49d19ecaa
Raido Kuli <raido357@gmail.com>
2022-01-14 10:58:26 +0200

    Rename repository

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module 'ember-refined-route-url/templates/*' {
+declare module 'ember-refined-route-substate-url/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
